ea61bc5e6a6659e667906008958cceb3478a579c
ZoeDev <84412530+ZoeDevE@users.noreply.github.com>
2021-11-01 21:03:18 +0100

    Removed old project

--- app/index.js
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
-import { Main } from './src/main';
-
-AppRegistry.registerComponent(appName, () => Main);
